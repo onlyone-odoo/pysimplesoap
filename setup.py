216a93cdc8d39cfb4be300304b7570aba88f4c0c
onlyone-odoo <matiasb@onlyone.com.ar>
2025-02-25 13:00:19 -0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,43 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 import os
 import subprocess
 import sys
 import warnings
 
-try:
-    import py2exe
-    from nsis import build_installer
-except:
-    build_installer = None
-
+# Importar versión y metadatos desde pysimplesoap
 from pysimplesoap import __version__, __author__, __author_email__, __license__
 
-# convert the README and format in restructured text (only when registering)
+# Convertir README.md a RST para la descripción larga (solo si es necesario)
 long_desc = ""
 if os.path.exists("README.md") and sys.platform == "linux2":
     try:
-        cmd = ['pandoc', '--from=markdown', '--to=rst', 'README.md']
+        cmd = ["pandoc", "--from=markdown", "--to=rst", "README.md"]
         long_desc = subprocess.check_output(cmd).decode("utf8")
         print("Long DESC", long_desc)
     except Exception as e:
         warnings.warn("Exception when converting the README format: %s" % e)
 
-
-# in the transition, register both:
-for name in ('soap2py', 'PySimpleSOAP'):
-    setup(
-        name=name,
-        version=__version__,
-        description='Python simple and lightweight SOAP Library',
-        long_description=long_desc,
-        author=__author__,
-        author_email=__author_email__,
-        url='http://code.google.com/p/pysimplesoap',
-        packages=['pysimplesoap'],
-        license=__license__,
-    #    console=['client.py'],
-        cmdclass={"py2exe": build_installer},
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-            "Topic :: Communications",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-    )
+# Definir un solo paquete
+setup(
+    name="PySimpleSOAP",
+    version=__version__,
+    description="Python simple and lightweight SOAP Library",
+    long_description=long_desc,
+    author=__author__,
+    author_email=__author_email__,
+    url="http://code.google.com/p/pysimplesoap",
+    packages=["pysimplesoap"],
+    install_requires=["httplib2"],  # Añadimos dependencias explícitas
+    license=__license__,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
